1e5ceb0cb34da114a23292aafecd900d4ed6badd
Vatsaev Aslan <a.vatsaev@gmail.com>
2017-11-10 23:34:13 +0100

    feat: functional approach in side effect chain

--- src/app/core/store/app.effects.ts
+++ src/app/core/store/app.effects.ts
@@ -8,37 +8,31 @@ import {Observable} from 'rxjs/Observable';
 import {Action} from '@ngrx/store';
 import {CryptoWidget, WeatherWidget, Widget} from '../models/widget';
 import * as appActions from '../store/app.actions';
+import 'rxjs/add/observable/empty'
 
 
 @Injectable()
-
 export class AppEffects {
 
   @Effect()
-  onUpdate$: Observable<Action> = this.actions$.ofType(appActions.UPDATE_WIDGET)
-    .pipe(
-      map((action: appActions.UpdateWidget) => action.payload),
-      mergeMap( (widget: Widget) => {
+  onUpdate$: Observable<Action> = this.actions$.ofType(appActions.UPDATE_WIDGET).pipe(
 
-        switch (widget.type) {
-          case 'crypto' : {
-            const w = widget as CryptoWidget;
-            return this.cryptoApi.getPrice(w.inCurrency, w.outCurrency).pipe(
-                map( price => new appActions.UpdateWidgetSuccess({id: w.id, price})
-              )
-            )
-          } 
-          case 'weather' : {
-            const w = widget as WeatherWidget;
-            return this.weatherApi.getCityWeather(w.city).pipe(
-                map( ({min, max}) => new appActions.UpdateWidgetSuccess({id: w.id, min, max})
-              )
-            )
-          }
-        }
-      })
-      
-    );
+    map((action: appActions.UpdateWidget) => action.payload),
+
+    mergeMap( (widget: Widget) =>
+
+      widget.type == 'crypto' ? this.cryptoApi.getPrice(...extractInOutCurrency(widget)).pipe(
+
+          map( (price: number) => mapToUpdateCryptoAction(widget, price))
+
+      ) : widget.type == 'weather' ? this.weatherApi.getCityWeather(extractCity(widget)).pipe(
+
+          map( ({min, max}) => mapToUpdateWeatherAction(widget, min, max))
+
+      ) : Observable.empty()
+    )
+
+  );
 
   constructor(
     private actions$: Actions,
@@ -46,3 +40,10 @@ export class AppEffects {
     private cryptoApi: CryptoCurrencyService
   ) {}
 }
+
+
+const extractInOutCurrency = (w: Widget) => [(w as CryptoWidget).inCurrency, (w as CryptoWidget).outCurrency];
+const extractCity = (w: Widget) => (w as WeatherWidget).city;
+
+const mapToUpdateWeatherAction = (widget: Widget, min: number, max: number) => new appActions.UpdateWidgetSuccess({id: widget.id, min, max} as Partial<WeatherWidget>);
+const mapToUpdateCryptoAction = (widget: Widget, price: number) => new appActions.UpdateWidgetSuccess({id: widget.id, price} as Partial<CryptoWidget>);
